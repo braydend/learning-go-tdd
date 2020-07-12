7f93f3e2476beab03d606c5d448d0a8b9fc5b8e3
Brayden <braydendiluca@gmail.com>
2020-07-12 19:28:40 +1000

    Update Sum to take array of any length of numbers

--- integers/integers.go
+++ integers/integers.go
@@ -4,7 +4,7 @@ func Add(x, y int) (result int) {
 	return x + y
 }
 
-func Sum(numbers [3]int) (result int) {
+func Sum(numbers []int) (result int) {
 	for _, number := range numbers {
 		result = result + number
 	}
--- integers/integers_test.go
+++ integers/integers_test.go
@@ -12,11 +12,24 @@ func TestAdd(t *testing.T) {
 }
 
 func TestSum(t *testing.T) {
-	numbers := [3]int{25, 35, 9}
-	result := Sum(numbers)
-	expected := 69
 
-	if expected != result {
-		t.Errorf("Expected: %d. Received: %d", expected, result)
-	}
+	t.Run("Sums array of 3 numbers", func(t *testing.T) {
+		numbers := []int{25, 35, 9}
+		result := Sum(numbers)
+		expected := 69
+
+		if expected != result {
+			t.Errorf("Expected: %d. Received: %d", expected, result)
+		}
+	})
+
+	t.Run("Sums array of any length of numbers", func(t *testing.T) {
+		numbers := []int{6, 10, 33, 2, 9, 9}
+		result := Sum(numbers)
+		expected := 69
+
+		if expected != result {
+			t.Errorf("Expected: %d. Received: %d", expected, result)
+		}
+	})
 }
