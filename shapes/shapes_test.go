c59bd5b1694d824f70d881ec626e82a393803b9d
Brayden <braydendiluca@gmail.com>
2020-07-19 11:04:51 +1000

    Add Triangle struct and add test case to table of tests

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -17,8 +17,9 @@ func TestArea(t *testing.T) {
 		shape    Shape
 		expected float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10.0}, 314.1592653589793},
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, testCase := range areaTests {
