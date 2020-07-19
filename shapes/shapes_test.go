b31fd92bd3c1fcc9a0de66b23f2e6fd518215f01
Brayden <braydendiluca@gmail.com>
2020-07-19 10:28:57 +1000

    Add Area func with tests

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -10,3 +10,12 @@ func TestPerimiter(t *testing.T) {
 		t.Errorf("Received: %.2f. Expected: %.2f", expected, result)
 	}
 }
+
+func TestArea(t *testing.T) {
+	result := Area(12.0, 6.0)
+	expected := 72.0
+
+	if expected != result {
+		t.Errorf("Received: %.2f. Expected: %.2f", result, expected)
+	}
+}
