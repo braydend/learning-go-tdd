bad739d71b88b128288e743dacbe50fdf9690089
Brayden <braydendiluca@gmail.com>
2020-07-19 10:33:07 +1000

    Add Rectangle struct and update funcs to take Rectangle arg

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -3,7 +3,8 @@ package shapes
 import "testing"
 
 func TestPerimiter(t *testing.T) {
-	result := Perimiter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	result := Perimiter(rectangle)
 	expected := 40.0
 
 	if expected != result {
@@ -12,7 +13,8 @@ func TestPerimiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	result := Area(12.0, 6.0)
+	rectangle := Rectangle{12.0, 6.0}
+	result := Area(rectangle)
 	expected := 72.0
 
 	if expected != result {
