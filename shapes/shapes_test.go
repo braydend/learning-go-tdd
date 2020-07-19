54f7c04e0dbace824d28e10229cb4128feb4b2cf
Brayden <braydendiluca@gmail.com>
2020-07-19 10:46:03 +1000

    Add Circle struct and refacter Area() to be a method on Rectangle and Circle

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -13,11 +13,23 @@ func TestPerimiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	result := Area(rectangle)
-	expected := 72.0
+	t.Run("Rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		result := rectangle.Area()
+		expected := 72.0
 
-	if expected != result {
-		t.Errorf("Received: %.2f. Expected: %.2f", result, expected)
-	}
+		if expected != result {
+			t.Errorf("Received: %.2f. Expected: %.2f", result, expected)
+		}
+	})
+
+	t.Run("Circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		result := circle.Area()
+		expected := 314.1592653589793
+
+		if expected != result {
+			t.Errorf("Received: %.2f. Expected: %.2f", result, expected)
+		}
+	})
 }
