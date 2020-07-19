1e71046291daa21f3985fa6a89bf5121ae9d08c7
Brayden <braydendiluca@gmail.com>
2020-07-19 11:11:58 +1000

    Update test cases to include name field, run inside t.Run() and print out struct value on failed test

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -14,19 +14,22 @@ func TestPerimiter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name     string
 		shape    Shape
 		expected float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, expected: 72.0},
-		{shape: Circle{Radius: 10}, expected: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, expected: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, expected: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, expected: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, expected: 36.0},
 	}
 
 	for _, testCase := range areaTests {
 		result := testCase.shape.Area()
 
-		if result != testCase.expected {
-			t.Errorf("Received: %.2f. Expected: %.2f", result, testCase.expected)
-		}
+		t.Run(testCase.name, func(t *testing.T) {
+			if result != testCase.expected {
+				t.Errorf("%#v. Received: %.2f. Expected: %.2f", testCase.shape, result, testCase.expected)
+			}
+		})
 	}
 }
