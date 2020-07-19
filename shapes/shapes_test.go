ab5d96a6fc81894e93f4ab71009f5e044de0863b
Brayden <braydendiluca@gmail.com>
2020-07-19 10:52:17 +1000

    Add Shape interface and checkShape func that uses the Shape interface

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -13,23 +13,26 @@ func TestPerimiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		result := rectangle.Area()
-		expected := 72.0
+	checkArea := func(t *testing.T, shape Shape, expected float64) {
+		t.Helper()
+		result := shape.Area()
 
 		if expected != result {
 			t.Errorf("Received: %.2f. Expected: %.2f", result, expected)
 		}
+	}
+
+	t.Run("Rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		expected := 72.0
+
+		checkArea(t, rectangle, expected)
 	})
 
 	t.Run("Circle", func(t *testing.T) {
 		circle := Circle{10.0}
-		result := circle.Area()
 		expected := 314.1592653589793
 
-		if expected != result {
-			t.Errorf("Received: %.2f. Expected: %.2f", result, expected)
-		}
+		checkArea(t, circle, expected)
 	})
 }
